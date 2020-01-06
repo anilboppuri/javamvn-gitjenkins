db949abc978a4d5524abb32472d87aacad859e3e
Anil Boppuri <techgig4you@gmail.com>
2020-01-06 11:28:16 +0530

    This is for demonstraton of new maven code

---
+++ src/main/java/com/mvn/git/javamvn_gitjenkins/Hello.java
@@ -0,0 +1,11 @@
+package com.mvn.git.javamvn_gitjenkins;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 System.out.println("hello this is from Maven Java project");
+	}
+
+}
